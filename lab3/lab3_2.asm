5f0e2fa018505cdf7fff5b15931dee053f635a2c
Rogerio <rogeriobatistijunior@gmail.com>
2025-09-30 21:40:55 -0300

    feat: add lab3 files

---
+++ lab3/lab3_2.asm
@@ -0,0 +1,72 @@
+.data
+    prompt_x:       .asciiz "Digite o valor de x (em radianos): "
+    result_msg:     .asciiz "\nSeno aproximado (20 termos): "
+    intermediate_msg: .asciiz "Soma apos termo "
+    colon_space:    .asciiz ": "
+    newline:        .asciiz "\n"
+    neg_one_d:      .double -1.0
+    zero_d:         .double 0.0
+
+.text
+
+main:
+    li      $v0, 4
+    la      $a0, prompt_x
+    syscall
+
+    li      $v0, 7
+    syscall
+
+    ldc1    $f12, zero_d
+    mov.d   $f14, $f0
+    
+    # Pré-calcular -x^2
+    mul.d   $f16, $f0, $f0
+    ldc1    $f28, neg_one_d
+    mul.d   $f16, $f16, $f28
+
+    # Estabelecendo precisao
+    li      $t0, 0
+    li      $t2, 20
+
+loop_start:
+    bge     $t0, $t2, exit
+
+    add.d   $f12, $f12, $f14    # sum = sum + term
+
+    # Mostra a convergência (resultado intermediário)
+    li      $v0, 4
+    la      $a0, intermediate_msg
+    syscall
+    li      $v0, 1
+    addiu   $a0, $t0, 1
+    syscall
+
+    li      $v0, 4
+    la      $a0, colon_space
+    syscall
+    li      $v0, 3
+    syscall
+    
+    li      $v0, 4
+    la      $a0, newline
+    syscall
+
+    addiu   $t0, $t0, 1
+    
+    sll     $t4, $t0, 1         
+    addiu   $t5, $t4, 1
+    mul     $t6, $t4, $t5
+
+    # Converte inteiro para double
+    mtc1    $t6, $f20
+    cvt.d.w $f20, $f20
+
+    div.d   $f22, $f16, $f20
+    mul.d   $f14, $f14, $f22
+    
+    j       loop_start
+
+exit:
+    li      $v0, 10
+    syscall
