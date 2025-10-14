1473bf12b8f2a8d99e5d163166c8649f27e27d82
Rogerio Batisti Junior <117864529+RBatisti@users.noreply.github.com>
2025-10-14 09:24:40 -0300

    feat: add ex1_v2

---
+++ lab5/ex1_v2.asm
@@ -0,0 +1,40 @@
+.data
+	text_1: .asciiz "Digite o limite do contador: "
+	text_2: .asciiz "Contador: "
+	new_line: .asciiz "\n"
+	
+.text
+main:
+	li $v0, 4
+	la $a0, text_1
+	syscall
+	
+	li $v0, 5
+	syscall
+	
+	move $s0, $v0 # store limit in $s0
+	
+	li $t0, 0 # initialize the counter
+	
+loop:
+	bge $t0, $s0, exit
+		
+	li $v0, 4
+	la $a0, text_2
+	syscall
+	
+	li $v0, 1
+	move $a0, $t0
+	syscall
+	
+	li $v0, 4
+	la $a0, new_line
+	syscall
+	
+	addi $t0, $t0, 2
+	
+	j loop
+	
+exit:
+	li $v0, 10
+	syscall
