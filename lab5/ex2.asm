24272830e1b45981434ef191a9f745b2fc54b691
Matheus Sebold <matheusssebold@gmail.com>
2025-10-13 21:36:34 -0300

    Update ex2.asm

--- lab5/ex2.asm
+++ lab5/ex2.asm
@@ -21,6 +21,7 @@ main:
 	li $v0, 9
 	syscall
 	move $s1, $v0
+	move $s7, $v0
 	
 	li $v0, 4
 	la $a0, text_2
@@ -48,22 +49,35 @@ loop_fill:
 	j loop_fill
 	
 exit_fill:
+	move $s2, $s0
+
 	li $v0, 4
-	la $s0, text_4
+	la $a0, text_4
 	syscall
 	
 	li $v0, 5
 	syscall
 	
-	move $s2, $a0
+	move $s2, $v0
 	
 	li $t0, 0 # found = 0
 	
-	li $t1, 0
+	li 	$t1, 0
+	
 	
 loop_search: # it is necessary to finish this procedure
-	beq $t1, $s0, exit_search
+	addi	$t2, $s7, 0
+	mul	$t3, $t1, 4
+	add	$t2, $s7, $t3
+	lw	$t2, 0($t2)
 	
+	bne	$t2, $s2, end_if
+	li	$t0, 1
+	j 	exit_search
+end_if:
+	addi	$t1, $t1, 1
+	beq 	$t1, $s0, exit_search
+	j loop_search
 	
 exit_search:
 	beqz $t0, not_found
@@ -71,14 +85,14 @@ exit_search:
 	
 not_found:
 	li $v0, 4
-	la $a0, text_5
+	la $a0, text_6
 	syscall
 	
 	j exit
 	
 found:
 	li $v0, 4
-	la $a0, text_6
+	la $a0, text_5
 	syscall
 	
 	j exit
