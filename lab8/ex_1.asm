850cd8a99c6d5fa979913e2cfe1848c3b096767c
Matheus Sebold <matheusssebold@gmail.com>
2025-11-12 13:46:20 -0300

    Comentarios

--- lab8/ex_1.asm
+++ lab8/ex_1.asm
@@ -1,77 +1,90 @@
 .data
+	# Define as constantes MAX (dimensão da matriz = 8) 
 	.eqv MAX 8
 
-	const_um: .float 1.0
-	const_dois: .float 2.0
+	constante_um: .float 1.0
+	constante_dois: .float 2.0
 
 .text
 main:
-	li $s4, MAX
+	li 		$s4, MAX
 
-	mul $t0, $s4, $s4
-	mul $a0, $t0, 4
+	mul 	$t0, $s4, $s4
+	mul 	$a0, $t0, 4
 
-	li $v0, 9
+	li 		$v0, 9
 	syscall 
-	move $s2, $v0
+	move 	$s2, $v0
 
-	li $v0, 9
+	li 		$v0, 9
 	syscall 
-	move $s3, $v0
+	move 	$s3, $v0
 
-	l.s $f10, const_um
-	l.s $f11, const_dois
+	l.s 	$f10, constante_um
+	l.s 	$f11, constante_dois
 
-	li $t1, 0
+	li 		$t1, 0
 
+	# --- Loop de Inicialização das Matrizes --- #
+	# Percorre todos os 64 elementos, definindo A[i] = 1.0 e B[i] = 2.0
 loop_init:
-	bge $t1, $t0, fim_loop_init
+	bge 	$t1, $t0, end_loop_init
 
-	mul $t2, $t1, 4
+	mul 	$t2, $t1, 4
 
-	add $t3, $s2, $t2
-	add $t4, $s3, $t2
+	add 	$t3, $s2, $t2
+	add 	$t4, $s3, $t2
 
-	s.s $f10, 0($t3)
-	s.s $f11, 0($t4)
+	s.s 	$f10, 0($t3)
+	s.s 	$f11, 0($t4)
 
-	addi $t1, $t1, 1
-	j loop_init
+	addi 	$t1, $t1, 1
+	j 		loop_init
 
-fim_loop_init:
-	li $s0, 0
+end_loop_init:
 	
+	# --- Início do Processamento Principal --- #
+	# Implementa o pseudocódigo:
+	# for (i = 0; i < MAX; i++) {
+	#   for (j = 0; j < MAX; j++) {
+	#     A[i][j] = A[i][j] + B[j][i];
+	#   }
+	# }
+	
+	li 		$s0, 0
 loop_i:
-	bge $s0, $s4, fim_loop_i
-	li $s1, 0
+	bge 	$s0, $s4, end_loop_i
+	li 		$s1, 0
 
 loop_j:
-	bge $s1, $s4, fim_loop_j
+	bge 	$s1, $s4, end_loop_j
 
-	# end A[i,j]
-	mul $t0, $s0, $s4
-	add $t0, $t0, $s1
-	mul $t0, $t0, 4
-	add $t1, $s2, $t0
+	# --- Cálculo do Endereço de A[i][j] --- #
+	mul 	$t0, $s0, $s4
+	add 	$t0, $t0, $s1
+	mul 	$t0, $t0, 4
+	add 	$t1, $s2, $t0
 
-	# end B[j,i]
-	mul $t2, $s1, $s4
-	add $t2, $t2, $s0
-	mul $t2, $t2, 4
-	add $t3, $s3, $t2
+	# --- Cálculo do Endereço de B[j][i] (Transposta) --- #
+	mul 	$t2, $s1, $s4
+	add 	$t2, $t2, $s0
+	mul 	$t2, $t2, 4
+	add 	$t3, $s3, $t2
 
-	# A[i,j] = A[i,j] + B[j,i]
-	l.s $f0, 0($t1)
-	l.s $f1, 0($t3)
-	add.s $f2, $f0, $f1
-	s.s $f2, 0($t1)
+	# --- Operação: A[i,j] = A[i,j] + B[j,i] --- #
+	l.s 	$f0, 0($t1)
+	l.s 	$f1, 0($t3)
+	add.s 	$f2, $f0, $f1
+	s.s 	$f2, 0($t1)
 
-	addi $s1, $s1, 1
-	j loop_j
-fim_loop_j:
-	addi $s0, $s0, 1
-	j loop_i
-fim_loop_i:
-	li $v0, 10
+	addi 	$s1, $s1, 1
+	j 		loop_j
+end_loop_j:
+	addi 	$s0, $s0, 1
+	j 		loop_i
+end_loop_i:
+
+	# --- Fim do Programa --- #
+	li 		$v0, 10
 	syscall
     
--- lab8/ex_2.asm
+++ lab8/ex_2.asm
@@ -1,91 +1,113 @@
 .data
+	# Define as constantes MAX (dimensão da matriz = 8) 
+	# e o tamanho do bloco para processamento (4x4)
 	.eqv MAX 8
-	.eqv tam_bloco 4
+	.eqv tamanho_bloco 4
 
-	const_um: .float 1.0
-	const_dois: .float 2.0
+	# Define constantes para inicialização das matrizes
+	constante_um: .float 1.0
+	constante_dois: .float 2.0
 
 .text
 
 main:
-	li $s6, MAX
-	li $s7, tam_bloco
+	li 		$s6, MAX
+	li 		$s7, tamanho_bloco
     
-	mul $t0, $s6, $s6
-	mul $a0, $t0, 4
+	mul 	$t0, $s6, $s6
+	mul 	$a0, $t0, 4
     
-	li $v0, 9
+	li 		$v0, 9
 	syscall 
-	move $s4, $v0
+	move 	$s4, $v0
     
-	li $v0, 9
+	li 		$v0, 9
 	syscall 
-	move $s5, $v0
+	move 	$s5, $v0
     
-	l.s $f10, const_um
-	l.s $f11, const_dois
+	l.s 	$f10, constante_um
+	l.s 	$f11, constante_dois
     
-	li $t1, 0
+	li 		$t1, 0
 
 loop_init:
-	bge $t1, $t0, fim_loop_init
+	bge 	$t1, $t0, end_loop_init
     
-	mul $t2, $t1, 4
-	add $t3, $s4, $t2
-	add $t4, $s5, $t2
-	s.s $f10, 0($t3)
-	s.s $f11, 0($t4)
-	addi $t1, $t1, 1
-	j loop_init
-fim_loop_init:
-	li $s0, 0
+	mul 	$t2, $t1, 4
+	add 	$t3, $s4, $t2
+	add 	$t4, $s5, $t2
+	s.s 	$f10, 0($t3)
+	s.s 	$f11, 0($t4)
+	addi 	$t1, $t1, 1
+	j 		loop_init
+end_loop_init:
+	li 		$s0, 0
+
+# --- Início do Processamento em Blocos --- #
+# O código implementa 4 loops aninhados para processar as matrizes em blocos.
+# Pseudocódigo (C):
+# for (i = 0; i < MAX; i += tamanho_bloco) {
+#   for (j = 0; j < MAX; j += tamanho_bloco) {
+#     for (ii = i; ii < i + tamanho_bloco; ii++) {
+#       for (jj = j; jj < j + tamanho_bloco; jj++) {
+#         A[ii][jj] = A[ii][jj] + B[jj][ii]; // Operação principal
+#       }
+#     }
+#   }
+# }
 
 loop_i:
-	bge $s0, $s6, fim_loop_i
-	li $s1, 0
+	bge 	$s0, $s6, end_loop_i
+	li 		$s1, 0
 	
 loop_j:
-	bge $s1, $s6, fim_loop_j
-	add $t0, $s0, $s7
-	move $s2, $s0
+	bge 	$s1, $s6, end_loop_j
+	add 	$t0, $s0, $s7
+	move 	$s2, $s0
 	
 loop_ii:
-	bge $s2, $t0, fim_loop_ii
-	add $t1, $s1, $s7
-	move $s3, $s1
+	bge 	$s2, $t0, end_loop_ii
+	add 	$t1, $s1, $s7
+	move 	$s3, $s1
 	
 loop_jj:
-	bge $s3, $t1, fim_loop_jj
+	bge 	$s3, $t1, end_loop_jj
 
-	# end A[ii,jj]
-	mul $t2, $s2, $s6
-	add $t2, $t2, $s3
-	mul $t2, $t2, 4
-	add $t3, $s4, $t2
+	# --- Núcleo do Cálculo --- #
+	# Operação: A[ii][jj] = A[ii][jj] + B[jj][ii]
 
-	# end B[jj,ii]
-	mul $t4, $s3, $s6
-	add $t4, $t4, $s2
-	mul $t4, $t4, 4
-	add $t5, $s5, $t4
+	# end A[ii,jj] (Cálculo do endereço de A[ii][jj])	mul 	$t2, $s2, $s6
+	add 	$t2, $t2, $s3
+	mul 	$t2, $t2, 4
+	add 	$t3, $s4, $t2
+
+	# end B[jj,ii] (Cálculo do endereço de B[jj][ii] - Acesso à transposta)
+	mul 	$t4, $s3, $s6
+	add 	$t4, $t4, $s2
+	mul 	$t4, $t4, 4
+	add 	$t5, $s5, $t4
 
 	# A[ii,jj] = A[ii,jj] + B[jj,ii]
-	l.s $f0, 0($t3)
-	l.s $f1, 0($t5)
-	add.s $f2, $f0, $f1
-	s.s $f2, 0($t3)
+	l.s 	$f0, 0($t3)
+	l.s 	$f1, 0($t5)
+	add.s 	$f2, $f0, $f1
+	s.s 	$f2, 0($t3)
 
-	addi $s3, $s3, 1
-	j loop_jj
-fim_loop_jj:
-	addi $s2, $s2, 1
-	j loop_ii
-fim_loop_ii:
-	add $s1, $s1, $s7
-	j loop_j
-fim_loop_j:
-	add $s0, $s0, $s7
-	j loop_i
-fim_loop_i:
-	li $v0, 10
+	# --- end do Núcleo --- #
+
+	addi 	$s3, $s3, 1
+	j 		loop_jj
+end_loop_jj:
+	addi 	$s2, $s2, 1
+	j 		loop_ii
+end_loop_ii:
+	add 	$s1, $s1, $s7
+	j 		loop_j
+end_loop_j:
+	add 	$s0, $s0, $s7
+	j 		loop_i
+end_loop_i:
+
+	# --- end do Programa --- #
+	li 		$v0, 10
 	syscall
